03ee6a3b53a0cf928e86f67b3d1910c674a13514
thembelihle22 <thembelihlegasa22@gmail.com>
2024-10-26 21:32:09 +0200

    injected javascript

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
+function searchDictionary(response) {
+  console.log(response.data);
+  let wordResult = document.querySelector("#h1");
+  wordResult.innerHTML = response.data[0].word;
+
+  let result = document.querySelector("#phonetic");
+  result.innerHTML = response.data[0].phonetics[1].text;
+
+  let speech = document.querySelector("#speech");
+  speech.innerHTML = response.data[0].meanings[0].partOfSpeech;
+
+  let define = document.querySelector("#defined");
+  define.innerHTML = response.data[0].meanings[0].definitions[0].definition;
+
+  let defined = document.querySelector("#second-defined");
+  defined.innerHTML = response.data[0].meanings[0].definitions[1].definition;
+}
+
 function dictionaryApi(word) {
-  let api = "https://api.dictionaryapi.dev/api/v2/entries/en/<word>";
+  let api = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
   axios.get(api).then(searchDictionary);
 }
+
+function wordSearchForm(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-box");
+  dictionaryApi(searchInput.value);
+}
+
+let button = document.querySelector("#search");
+button.addEventListener("submit", wordSearchForm);
